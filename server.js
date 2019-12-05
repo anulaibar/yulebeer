352267a7f80cc780bf38e8a9b86e43b8192475e2
Olle Hellgren <olle.hellgren@blocket.se>
2019-12-05 01:17:42 +0100

    Update path to static dir for express; add favicon

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post("/api/votes", (req, res) => {
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", function(request, response) {
-  response.sendFile(path.resolve(__dirname, "index.html"));
+  response.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
