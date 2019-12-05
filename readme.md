005802a153068e1b157cde8776d1d63efc9baaae
Olle Hellgren <olle.hellgren@blocket.se>
2019-12-05 10:30:08 +0100

    Add beers, add open graph meta tags for link sharing preview

--- readme.md
+++ readme.md
@@ -1,23 +1,47 @@
 # Yule Beer
+
 A web app for beer ratings
 
 ### Install
+
+Create db and install deps:
 ```
 createdb yule_beer
 npm i
 ```
 
 ### Quickstart
+
+Start express:
 ```
 npm start
 ```
 
+Start dev server:
+```
+npm run dev
+```
+
+Start in production mode:
+```
+heroku local
+```
+
 ### Scrape
+
+Scrape systembolaget.se and populate local db:
 ```
 npm run scrape
 ```
 
+Populate production db:
+```
+NODE_ENV=production DATABASE_URL=$(heroku config:get DATABASE_URL) node scrape.js
+```
+
 ### Db
+
+Connect to local db:
 ```
 psql yule_beer
 ```
--- scrape.js
+++ scrape.js
@@ -27,12 +27,16 @@ const scrapeBeer = async url => {
 };
 
 const urls = [
-  "https://www.systembolaget.se/dryck/ol/jamtlands-julol-1138203",
-  "https://www.systembolaget.se/dryck/ol/lagunitas-holiday-ale-1131203",
   "https://www.systembolaget.se/dryck/ol/nice-chouffe-1136401",
-  "https://www.systembolaget.se/dryck/ol/hibernation-ale-1133203",
+  "https://www.systembolaget.se/dryck/ol/gouden-carolus-1129903",
   "https://www.systembolaget.se/dryck/ol/wonderful-winter-ale-1147515",
-  "https://www.systembolaget.se/dryck/ol/gouden-carolus-1129903"
+  "https://www.systembolaget.se/dryck/ol/hibernation-ale-1133203",
+  "https://www.systembolaget.se/dryck/ol/charlis-snofall-1126903",
+  "https://www.systembolaget.se/dryck/ol/st-eriks-1136203",
+  "https://www.systembolaget.se/dryck/ol/daulne-1129303",
+  "https://www.systembolaget.se/dryck/ol/train-station-brewery-1132203",
+  "https://www.systembolaget.se/dryck/ol/goteborgs-christmas-1072103",
+  "https://www.systembolaget.se/dryck/ol/ballast-point-1117903"
 ];
 
 for (let url of urls) {
