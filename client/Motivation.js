ffef107b694dbd97b12751d18afaac7529e6cc4a
Olle Hellgren <olle@rebla.se>
2020-11-12 13:10:06 +0100

    Style text area

--- client/Motivation.js
+++ client/Motivation.js
@@ -20,35 +20,32 @@ const Motivation = () => {
   }, [done]);
 
   return (
-    <div
+    <textarea
+      onBlur={() => setDone(true)}
+      onFocus={() => setDone(false)}
+      value={state}
+      onChange={e => setState(e.target.value)}
       css={css`
         margin-top: 16px;
+        color: sienna;
+        font-size: 16px;
+        font-family: "Open Sans", sans-serif;
         width: 100%;
         box-sizing: border-box;
+        resize: none;
+        outline: none;
+        border: none;
+        padding: 32px;
+        box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+          0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+          0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+        border-radius: 5px;
+        background-color: white;
       `}
-    >
-      <textarea
-        onBlur={() => setDone(true)}
-        onFocus={() => setDone(false)}
-        value={state}
-        onChange={e => setState(e.target.value)}
-        css={css`
-          padding: 16px;
-          width: 100%;
-          box-sizing: border-box;
-          border-radius: 5px;
-          resize: none;
-          outline: none;
-          border: none;
-          box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
-            0px 1px 1px 0px rgba(0, 0, 0, 0.14),
-            0px 1px 3px 0px rgba(0, 0, 0, 0.12);
-        `}
-        type="text"
-        multiline
-        rows={7}
-      />
-    </div>
+      type="text"
+      multiline
+      rows={7}
+    />
   );
 };
 
